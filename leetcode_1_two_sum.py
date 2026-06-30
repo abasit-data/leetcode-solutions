d9319466983b3b5472f940c8c5a5cb81e1beb72c
Abdul Basit <engrbasit8@gmail.com>
2026-06-30 12:18:44 +0500

    Update leetcode_1_two_sum.py

--- leetcode_1_two_sum.py
+++ leetcode_1_two_sum.py
@@ -1,20 +1,6 @@
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        notebook = {} 
-        
-        for i, number in enumerate(nums):
-            friend_we_need = target - number
-            
-            # --- THE SEARCH ---
-            if friend_we_need in notebook:
-                # We found the index of the friend in our notebook
-                index_of_friend = notebook[friend_we_need]
-                # Return both indices as a list
-                return [index_of_friend, i]
-            
-            # --- THE STORAGE ---
-            # If no friend found, store CURRENT number and its index
-            # This allows future numbers in the loop to find THIS number
-            notebook[number] = i
-            
-        return [] # Empty list if no pair exists
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if target - nums[i] == nums[j]:
+                    return [i, j]
